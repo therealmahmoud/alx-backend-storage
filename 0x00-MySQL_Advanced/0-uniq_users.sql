03572e5a656c8fed424e0a869dc529939d902bc4
therealmahmoud <moha91875460@gmail.com>
2024-07-11 13:26:04 +0300

    Updated

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,4 @@
+-- Creating table in holberton db
+CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                                email VARCHAR(255) NOT NULL UNIQUE,
+                                NAME VARCHAR(255));
