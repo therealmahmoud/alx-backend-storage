dcb831e8bfcf13942e2afbc90e58e1ad5a094928
therealmahmoud <moha91875460@gmail.com>
2024-07-12 00:10:25 +0300

    Updated

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- SQL script to list Glam rock bands ranked by longevity
-SELECT
-    name AS band_name,
+SELECT name AS band_name,
     CASE
         WHEN split IS NULL THEN 2022 - formed
         ELSE split - formed
