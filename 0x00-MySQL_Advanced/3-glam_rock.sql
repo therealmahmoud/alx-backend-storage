042a86cd102f51432276ce8c9b6e9c9c40e86c2e
therealmahmoud <moha91875460@gmail.com>
2024-07-11 22:30:27 +0300

    Updated

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- lists all bands with Glam rock as their main style
-SELECT band_name, (IFNULL(split, '2020') - formed) AS life_span FROM metal_bands
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY life_span DESC;
+ORDER BY lifespan DESC;
