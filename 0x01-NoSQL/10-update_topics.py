9565ba0182caaaedc7566e75d51664fc7e8f4a68
therealmahmoud <moha91875460@gmail.com>
2024-07-17 02:14:16 +0300

    Updated

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,5 +3,6 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name."""
     res = mongo_collection.update({name}, [topics])
     return res
